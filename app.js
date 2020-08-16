ed3da6dd9a8913ff2dcbb729ed61102d8374e1c5
ehwus <awithsmith@tutanota.com>
2020-08-16 11:22:47 +0100

    added the ability to download using FileSaver, helloworld placeholder

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ function compile() {
 }
 
 function save() {
-    alert("Todo!");
+    var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "hello world.txt");
 }
 
 document.getElementById("save").addEventListener("click", save);
